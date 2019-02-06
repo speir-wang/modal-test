85b4e18ff06d8460decbec66c182e769d690d043
He Wang <speir.wang@gmail.com>
2019-02-06 22:43:09 +1100

    project clean up

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+/**
+ * Modal structure: header -> body -> footer
+ *
+ * Modal body:
+ *  left: <TastList>
+ *  right: <UserInput>
+ */
+
+class App extends Component {
+	render() {
+		return <div>App</div>;
+	}
+}
+
+export default App;
